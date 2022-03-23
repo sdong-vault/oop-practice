34dab5141cdf5ee9117cfb299cfac41d5c8e3e43
sdong-vault <sdong@vaulthealth.com>
2022-03-23 09:21:10 -0400

    Convert battle system to an engine

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -203,29 +203,38 @@ def create_trainer_2():
     return trainer
 
 
-def main():
-    unit_tests()
+class BattleEngine:
+    def __init__(self, battler1, battler2):
+        self.turn_order = [
+            {
+                "battler": battler1,
+                "status": "good"
+            },
+            {
+                "battler": battler2,
+                "status": "good"
+            },
+        ]
+        self.turn_index = 0
 
-    trainer1 = create_trainer_1()
-    trainer2 = create_trainer_2()
+    def increment_turn_order(self):
+        self.turn_index = (self.turn_index + 1) % len(self.turn_order)
 
-    turn_order = [trainer1, trainer2]
-    turn_index = 0
-    def increment_turn_order():
-        return (turn_index + 1) % len(turn_order)
-    def get_current_trainer():
-        return turn_order[turn_index]
+    def get_current_battler(self):
+        return self.turn_order[self.turn_index]
+    
+    def get_current_opponent(self):
+        opponent_index = (self.turn_index + 1) % len(self.turn_order)
+        return self.turn_order[opponent_index]
+    
+    def change_current_battler_status(self, status):
+        self.turn_order[self.turn_index]["status"] = status
+    
+    def prompt_current_battler(self):
+        battler_info = self.get_current_battler()
+        battler = battler_info["battler"]
 
-    while True:
-        trainer = get_current_trainer()
-
-        if not trainer.has_remaining_pokemon():
-            print(f"Trainer {trainer.name} does not have anymore pokemon to fight!")
-            print(f"Trainer {trainer.name} has lost the match!")
-            turn_index = increment_turn_order()
-            break
-
-        print(f"It is {trainer.name}'s turn!")
+        print(f"It is {battler.name}'s turn!")
         print(f"What would you like to do?")
 
         print(f"1. Attack")
@@ -236,15 +245,35 @@ def main():
         if response == "attack":
             print("attack!!!")
         else:
-            print(f"Trainer {trainer.name} has forfeited the match!")
-            print(f"Trainer {trainer.name} has lost the match!")
-            turn_index = increment_turn_order()
-            break
-
-        turn_index = increment_turn_order()
+            print(f"Trainer {battler.name} has forfeited the match!")
+            self.change_current_battler_status("lost")
     
-    trainer = get_current_trainer()
-    print(f"Trainer {trainer.name} has won!")
+    def determine_winner(self):
+        if self.get_current_battler()["status"] == "lost":
+            return self.get_current_opponent()["battler"]
+        elif self.get_current_opponent()["status"] == "lost":
+            return self.get_current_battler()["battler"]
+        else:
+            return None
+
+
+def main():
+    unit_tests()
+
+    battle_engine = BattleEngine(
+        create_trainer_1(),
+        create_trainer_2()
+    )
+
+    winner = None
+    while winner is None:
+        battle_engine.prompt_current_battler()
+
+        battle_engine.increment_turn_order()
+
+        winner = battle_engine.determine_winner()
+    
+    print(f"Trainer {winner.name} has won!")
 
 
 
